9a3d6e9935f0461f3a343fa5498d101462180f50
ShuvoSW <shuvomajumder3369@gmail.com>
2026-04-06 00:22:40 +0600

    Implement Login form

---
+++ frontend/src/lib/zod/auth.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Minimum 6 characters"),
+});
+
+export const registerSchema = z.object({
+  name: z.string().min(3, "Minimum 3 characters"),
+  email: z.string().email("Invalid email"),
+  password: z.string().min(6, "Minimum 6 characters"),
+  confirmPassword: z.string(),
+  image: z.string().optional(),
+});
